f8e04bda43d926c755714abdaa86b84ef523b1f4
Wonjun <wonjune12@hotmail.com>
2019-01-28 15:37:06 -0500

    removed ENV

--- app.rb
+++ app.rb
@@ -66,8 +66,7 @@ post "/catalogsent" do
 
 
   mail = SendGrid::Mail.new(from, subject, to, content)
-  sg = SendGrid::API.new( api_key: ENV['SENDGRID_API_KEY'])
-
+  sg = SendGrid::API.new()
   response = sg.client.mail._('send').post(request_body: mail.to_json)
   
   response.status_code
